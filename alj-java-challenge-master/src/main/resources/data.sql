63576b2133dc8fc1d08aeadc11be086d29dfcf8c
thamizhselvan <85830822+thamizhselvananbu@users.noreply.github.com>
2023-02-12 12:58:29 +0900

    Add data and schema sql files for H2 database
    Create table with data while loading application

---
+++ alj-java-challenge-master/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO EMPLOYEE(`EMPLOYEE_NAME`,`EMPLOYEE_SALARY`, `DEPARTMENT`)
+VALUES
+  ('Leif Weyand',100000, 'IT'),
+  ('Claris Blackford',50000, 'NON-IT'),
+  ('Lessie Grear',50000, 'NON-IT'),
+  ('Reyes Callaghan',50000, 'NON-IT'),
+  ('Herb Relyea',200000, 'IT'),
+  ('Gale Klingenberg',200000, 'IT'),
+  ('Margene Lore',200000, 'IT'),
+  ('Demarcus Glassford',300000, 'IT'),
+  ('Anamaria Manthe',300000, 'IT'),
+  ('Farrah Lanphere',300000, 'IT');
