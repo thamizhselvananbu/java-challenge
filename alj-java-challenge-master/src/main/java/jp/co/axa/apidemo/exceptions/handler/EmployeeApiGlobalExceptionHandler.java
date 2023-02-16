46623bb5b3224ab1290daf0a00f35b2d8d6d686b
thamizhselvan <85830822+thamizhselvananbu@users.noreply.github.com>
2023-02-17 00:17:16 +0900

    enhanced exception handler class

--- alj-java-challenge-master/src/main/java/jp/co/axa/apidemo/exceptions/handler/EmployeeApiGlobalExceptionHandler.java
+++ alj-java-challenge-master/src/main/java/jp/co/axa/apidemo/exceptions/handler/EmployeeApiGlobalExceptionHandler.java
@@ -4,8 +4,6 @@ import jp.co.axa.apidemo.exceptions.EmployeeApiException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,7 +11,6 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -21,28 +18,46 @@ import java.util.Map;
 @ControllerAdvice
 public class EmployeeApiGlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    private static final String TIMESTAMP = "timestamp";
+
+    private static final String STATUS = "status";
+
+    private static final String ERROR = "error";
+
+    private static final String MESSAGE = "message";
+
     @ExceptionHandler(value = {EmployeeApiException.class})
     public EmployeeApiException handleEmployeeApiExceptions(EmployeeApiException apiException, WebRequest request) {
         return apiException;
     }
 
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<Object> handleApiExceptions(EmployeeApiException apiException, WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put(TIMESTAMP, LocalDateTime.now());
+        body.put(STATUS, HttpStatus.INTERNAL_SERVER_ERROR.value());
+        body.put(ERROR, "Internal server error");
+        body.put(MESSAGE,"Something went wrong");
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @ExceptionHandler(value = {MethodArgumentTypeMismatchException.class})
     public ResponseEntity<Object> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex, WebRequest request) {
         Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.BAD_REQUEST.value());
-        body.put("error", "Invalid argument type");
-        body.put("message","Argument type mismatched");
+        body.put(TIMESTAMP, LocalDateTime.now());
+        body.put(STATUS, HttpStatus.BAD_REQUEST.value());
+        body.put(ERROR, "Invalid argument type");
+        body.put(MESSAGE,"Argument type mismatched");
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
     @Override
     protected ResponseEntity<Object> handleMissingPathVariable(MissingPathVariableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.BAD_REQUEST.value());
-        body.put("error", "Required value is missing");
-        body.put("message","Required value is not present in request URL");
+        body.put(TIMESTAMP, LocalDateTime.now());
+        body.put(STATUS, HttpStatus.BAD_REQUEST.value());
+        body.put(ERROR, "Required value is missing");
+        body.put(MESSAGE,"Required value is not present in request URL");
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 }
