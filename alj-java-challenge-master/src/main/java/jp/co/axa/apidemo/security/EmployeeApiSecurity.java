bd00a0853e7e54e3123b7f62a7bceddcb584a096
thamizhselvan <85830822+thamizhselvananbu@users.noreply.github.com>
2023-02-17 00:17:36 +0900

    basic authentication using spring security

---
+++ alj-java-challenge-master/src/main/java/jp/co/axa/apidemo/security/EmployeeApiSecurity.java
@@ -0,0 +1,29 @@
+package jp.co.axa.apidemo.security;
+
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class EmployeeApiSecurity extends WebSecurityConfigurerAdapter implements ApplicationContextAware {
+
+    // Secure the endpoints with HTTP Basic authentication
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                //HTTP Basic authentication
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/v1/employees/**").permitAll()
+                .antMatchers("/api/v1/employees/**").authenticated()
+                .and()
+                .csrf().disable()
+                .formLogin().disable();
+    }
+}
