df17b86ec4f1f118621f3e4468203f1c8078d1a6
thamizhselvan <85830822+thamizhselvananbu@users.noreply.github.com>
2023-02-17 13:09:56 +0900

    Added comments and formatted validatorUtills class

--- alj-java-challenge-master/src/main/java/jp/co/axa/apidemo/validatorutils/ValidatorUtils.java
+++ alj-java-challenge-master/src/main/java/jp/co/axa/apidemo/validatorutils/ValidatorUtils.java
@@ -28,13 +28,18 @@ public final class ValidatorUtils {
         return object == null;
     }
 
+    /**
+     * Validate employee request sent from request body
+     * employee name,salary and department should not be null
+     * employeeId will be auto generated if it is not exists
+     * employee salary should not be less than 0
+     */
     public static void validateEmployee(EmployeeRequest employeeRequest) {
-            if (isNull(employeeRequest.getName())
-                    || employeeRequest.getSalary() <= 0
-                    || isNull(employeeRequest.getDepartment())
-            ) {
-                throw new EmployeeApiException("Invalid Employee json");
-            } else {
+        if (isNull(employeeRequest.getName())
+                || employeeRequest.getSalary() <= 0
+                || isNull(employeeRequest.getDepartment())) {
+            throw new EmployeeApiException("Invalid Employee json");
+        } else {
             throw new EmployeeApiException("Employee should not be null");
         }
     }
